989d16e95b719a5a43cfaf145f30fd0f43372a53
João Batista Gomes Silva <jhonxbatista@gmail.com>
2023-11-08 16:44:01 -0300

    feat: adicionando anuncios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import AudioPlayer from './components/AudioPlayer.vue';
-import Adds from './components/Adds.vue';
 import RRS from './components/RRS.vue';
 
 import Logo from './assets/logo.png';
@@ -12,8 +11,6 @@ import Logo from './assets/logo.png';
       <img :src="Logo" alt="" class="w-[200px]" />
       <audio-player />
     </div>
-
-    <adds />
 
     <div class="fixed top-0">
       <RRS />
--- src/components/Adds.vue
+++ src/components/Adds.vue
@@ -2,39 +2,7 @@
   <div class="max-w-[200px] md:max-w-[600px] mx-auto my-8">
     <div
       class="flex flex-col justify-center md:justify-normal md:grid md:grid-cols-4 gap-2"
-    >
-      <div
-        class="p-4 border-2 rounded-lg border-white aspect-square flex items-center justify-center"
-      >
-        <h3 class="text-white text-center uppercase font-bold">
-          Anuncie aqui sua marca
-        </h3>
-      </div>
-
-      <div
-        class="p-4 border-2 rounded-lg border-white aspect-square flex items-center justify-center"
-      >
-        <h3 class="text-white text-center uppercase font-bold">
-          Anuncie aqui sua marca
-        </h3>
-      </div>
-
-      <div
-        class="p-4 border-2 rounded-lg border-white aspect-square flex items-center justify-center"
-      >
-        <h3 class="text-white text-center uppercase font-bold">
-          Anuncie aqui sua marca
-        </h3>
-      </div>
-
-      <div
-        class="p-4 border-2 rounded-lg border-white aspect-square flex items-center justify-center"
-      >
-        <h3 class="text-white text-center uppercase font-bold">
-          Anuncie aqui sua marca
-        </h3>
-      </div>
-    </div>
+    ></div>
   </div>
 </template>
 
